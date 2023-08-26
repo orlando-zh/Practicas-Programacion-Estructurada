1d1fc6e6e1996f680fe8c8c499bd405b3ce579ed
orlando-zh <orlandozuniga4@gmail.com>
2023-08-25 18:06:07 -0600

    subiendo

---
+++ EJERCICIO 1/index.js
@@ -0,0 +1,5 @@
+const nombresAnimales = ["Perro", "Gato", "Elefante", "León", "Tigre", "Jirafa"];
+
+// Mostrar el arreglo en la consola
+console.log(nombresAnimales);
+
---
+++ EJERCICIO 5/index5.js
@@ -0,0 +1,7 @@
+let colonias = ["Shinganshina", "Subterránea", "Mitras", "Klorva", "SNK"];
+
+// Obteniendo el tamaño del arreglo usando la propiedad length
+const tamanoArreglo = colonias.length;
+
+// Mostrar un mensaje con el tamaño del arreglo
+console.log(`El arreglo contiene ${tamanoArreglo} colonias.`);
