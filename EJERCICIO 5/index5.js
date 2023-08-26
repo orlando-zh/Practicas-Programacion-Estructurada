18c27a525658e979ba61c7dc8ec80bea4b90e66a
Orlando Zúniga <139894785+orlando-zh@users.noreply.github.com>
2023-08-25 18:23:34 -0600

    Delete EJERCICIO 5 directory

--- EJERCICIO 5/index5.js
+++
@@ -1,7 +0,0 @@
-let colonias = ["Shinganshina", "Subterránea", "Mitras", "Klorva", "SNK"];
-
-// Obteniendo el tamaño del arreglo usando la propiedad length
-const tamanoArreglo = colonias.length;
-
-// Mostrar un mensaje con el tamaño del arreglo
-console.log(`El arreglo contiene ${tamanoArreglo} colonias.`);
