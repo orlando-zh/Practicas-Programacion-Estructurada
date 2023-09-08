38ae3d91ea37d47f827414dc884c765a083efe0d
Orlando Zúniga <139894785+orlando-zh@users.noreply.github.com>
2023-09-08 11:10:44 -0600

    Add files via upload

---
+++ PRACTICA 1/script.js
@@ -0,0 +1,58 @@
+//alert("Mensaje de alerta en javascript!!")
+
+
+console.log("Estas usando la consola")
+console.log(20)
+console.log(1.2)
+console.log(true)
+console.log(false)
+
+
+
+//_________________________________________________________________________//
+
+/* Mostrar por consola tu nombre y edad -  Mostrar un mensaje de alerta*/
+
+
+/*let nombre = "Orlando Zúniga"
+let edad = 19
+let mensaje = "Mi nombre es " + nombre + " y tengo " + edad
+
+alert(mensaje)*/
+
+//_________________________________________________________________________//
+
+
+
+/*Programa que genere una tabla de multiplicar basada en el número 
+ingresado por el usuario. Deberás usar las funciones prompt y un bucle for 
+para lograrlo*/
+
+
+
+/*const numero = parseInt(prompt("Ingrese un numero para generar su tala de multipicar"));
+
+console.log(`Tabla de multiplicar del ${numero}`);
+
+for (let i = 1; i<=15; i++){
+    const resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`)
+}/*
+
+
+
+//_________________________________________________________________________//
+
+
+/*Programa que solicite al usuario ingresar un número utilizando prompt, y 
+luego utiliza la estructura if para evaluar si el número es positivo, negativo o 
+cero. Dependiendo del valor ingresado, se imprimirá el mensaje 
+correspondiente en la consola*/
+
+/*if(numero > 0){
+    console.log("El nunmero es positivo")
+}else if(numero < 0){
+    console.log("El numero es negativo")
+}else{
+    console.log("No es valido")
+}/*
