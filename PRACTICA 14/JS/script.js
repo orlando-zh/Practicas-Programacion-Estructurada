d1fb8bff30552c3a1b6078fb215d54cf8a99c979
Orlando Zúniga <139894785+orlando-zh@users.noreply.github.com>
2023-12-07 20:56:10 -0600

    Create script.js

---
+++ PRACTICA 14/JS/script.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var painting = false;
+
+ctx.scale(2, 2);
+
+function startPosition(e) {
+    painting = true;
+    draw(e);
+}
+
+function endPosition() {
+    painting = false;
+    ctx.beginPath();
+}
+
+function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 15;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = document.getElementById('colorPicker').value;
+    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+}
+
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', endPosition);
+canvas.addEventListener('mousemove', draw);
